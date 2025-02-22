c11bd2dc0d1ff487a99f83a10ad8796ba1e5c245
AgungDH <agungdh@live.com>
2025-02-22 20:53:15 +0700

    init

---
+++ src/main/java/id/my/agungdh/linechat/LinechatApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.linechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinechatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/linechat/LinechatApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.linechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

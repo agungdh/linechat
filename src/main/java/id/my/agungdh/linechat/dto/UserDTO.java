b8e42b664c492f717ca29a30161fa1eacf6d4672
AgungDH <agungdh@live.com>
2025-02-23 00:17:07 +0700

    change not null to not blank

--- src/main/java/id/my/agungdh/linechat/dto/UserDTO.java
+++ src/main/java/id/my/agungdh/linechat/dto/UserDTO.java
@@ -1,18 +1,19 @@
 package id.my.agungdh.linechat.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserDTO(
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         Long id,
-        @NotNull
+        @NotBlank
         String username,
-        @NotNull
+        @NotBlank
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
         String password,
-        @NotNull
+        @NotBlank
         String name
 ) {
 }

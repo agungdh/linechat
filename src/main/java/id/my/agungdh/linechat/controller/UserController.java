0e095f5913b327b1eed06877c394fe89e66fba1c
AgungDH <agungdh@live.com>
2025-02-23 15:10:19 +0700

    lint

--- src/main/java/id/my/agungdh/linechat/controller/UserController.java
+++ src/main/java/id/my/agungdh/linechat/controller/UserController.java
@@ -5,7 +5,6 @@ import id.my.agungdh.linechat.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

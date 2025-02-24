fa40286799b6cbab0326d6886a0ca666fb410086
AgungDH <agungdh@live.com>
2025-02-24 11:27:41 +0700

    lint

--- src/main/java/id/my/agungdh/linechat/exception/GlobalExceptionHandler.java
+++ src/main/java/id/my/agungdh/linechat/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package id.my.agungdh.linechat.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
